c9e4ac1d2021d411e9361ea035f0d93e00016629
Laure Guery <laureguery@hotmail.com>
2016-04-18 16:30:13 +0100

    add change

--- hwsum.js
+++ hwsum.js
@@ -1,12 +1,16 @@
 
             var event1 = document.getElementById("button1");
-            event1.addEventListener ('click',myFirst,false);
+            event1.addEventListener('click', myFirst, false);
             function myFirst (){
                 
           var userinput1 = parseInt(document.getElementById("intput1").value);
           var userinput2 = parseInt(document.getElementById("intput2").value);      
           var ans = userinput1 + userinput2;
-          ans>5, true
+                
+                if(input1 < input2){
+                   document.getElementById("answer") = input2
+                }
+         
           document.getElementById("answer").innerHTML = ans; 
            
 
