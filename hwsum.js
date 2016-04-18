9289102297ea0fde7463fd8521c2904be4e8fc30
Laure Guery <laureguery@hotmail.com>
2016-04-18 16:13:29 +0100

    add to file

---
+++ hwsum.js
@@ -0,0 +1,16 @@
+
+            var event1 = document.getElementById("button1");
+            event1.addEventListener ('click',myFirst,false);
+            function myFirst (){
+                
+          var userinput1 = parseInt(document.getElementById("intput1").value);
+          var userinput2 = parseInt(document.getElementById("intput2").value);      
+          var ans = userinput1 + userinput2;
+          ans>5, true
+          document.getElementById("answer").innerHTML = ans; 
+           
+
+           
+                
+            }
+
